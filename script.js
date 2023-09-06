c4bde400981e71950bad66f7196f41e54a4b7478
Leticia Moraes <leticiaemilymoraes14@gmail.com>
2023-09-06 15:45:16 -0300

    Alteração no slider
    Foi alterado o slider, colocando um time para mudança e um transição

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const slider = document.querySelector('.slider');
 const slide = document.querySelectorAll('.slide');
-const indicadores = document.querySelectorAll('.indicadores');
+const indicadores = document.querySelectorAll('.indicador');
 const setaAnterior = document.querySelector('.seta-anterior');
 const setaProxima = document.querySelector('.seta-proxima');
 
 let currentIndex = 0;
+let interval;
 
 function criarIndicadores() {
     slide.forEach((_, index) => {
@@ -26,26 +27,38 @@ function atualizarIndicadores() {
     });
 }
 
-function atualizarslider() {
+function atualizarSlider() {
     const slideWidth = slide[0].clientWidth;
     slider.style.transform = `translateX(-${currentIndex * (slideWidth + 20)}px)`;
-    atualizarIndicadores()
+    atualizarIndicadores();
 }
 
 function slidePara(index) {
     currentIndex = index;
-    atualizarslider()
+    atualizarSlider();
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slide.length;
+    atualizarSlider();
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slide.length) % slide.length;
+    atualizarSlider();
 }
 
 setaAnterior.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slide.length) % slide.length;
-    atualizarslider()
+    prevSlide();
+    clearInterval(interval);
+    interval = setInterval(nextSlide, 10000);
 });
 
 setaProxima.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slide.length;
-    atualizarslider()
+    nextSlide();
+    clearInterval(interval);
+    interval = setInterval(nextSlide, 10000);
 });
 
 criarIndicadores();
-atualizarslider()
+interval = setInterval(nextSlide, 10000);
